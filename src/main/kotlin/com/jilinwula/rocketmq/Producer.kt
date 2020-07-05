da9a2913cb568f5d56b60e82a61cfdaae6cc7bfb
jilinwula <admin@jilinwula.com>
2020-07-05 17:52:49 +0800

    RocketMQ入门

--- src/main/kotlin/com/jilinwula/rocketmq/Producer.kt
+++ src/main/kotlin/com/jilinwula/rocketmq/Producer.kt
@@ -8,7 +8,7 @@ class Producer
 
 fun main(){
     val producer = DefaultMQProducer("producerGroup-test")
-    producer.namesrvAddr = "47.95.201.47:9876"
+    producer.namesrvAddr = "47.95.201.37:9876"
     producer.start()
 
     var message = Message("topic-test","tag-test","key-test","hello world test".toByteArray())
