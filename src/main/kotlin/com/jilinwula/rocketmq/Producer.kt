db5140b7a5f4b40fe454dc40ed96991eab765bc8
jilinwula <admin@jilinwula.com>
2020-07-05 16:56:26 +0800

    RocketMQ入门

---
+++ src/main/kotlin/com/jilinwula/rocketmq/Producer.kt
@@ -0,0 +1,20 @@
+package com.jilinwula.rocketmq
+
+import com.alibaba.fastjson.JSONObject
+import org.apache.rocketmq.client.producer.DefaultMQProducer
+import org.apache.rocketmq.common.message.Message
+
+class Producer
+
+fun main(){
+    val producer = DefaultMQProducer("producerGroup-test")
+    producer.namesrvAddr = "47.95.201.47:9876"
+    producer.start()
+
+    var message = Message("topic-test","tag-test","key-test","hello world test".toByteArray())
+
+    var send = producer.send(message)
+
+    print(JSONObject.toJSONString(send))
+
+}
