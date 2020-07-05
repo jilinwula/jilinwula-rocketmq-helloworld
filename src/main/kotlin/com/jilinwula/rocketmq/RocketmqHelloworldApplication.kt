075eb8601dc07bcc954be86e8cce3b7dedaea12a
jilinwula <admin@jilinwula.com>
2020-07-05 15:37:07 +0800

    RocketMQ入门

---
+++ src/main/kotlin/com/jilinwula/rocketmq/RocketmqHelloworldApplication.kt
@@ -0,0 +1,11 @@
+package com.jilinwula.rocketmq
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RocketmqHelloworldApplication
+
+fun main(args: Array<String>) {
+    runApplication<RocketmqHelloworldApplication>(*args)
+}
---
+++ src/test/kotlin/com/jilinwula/rocketmq/RocketmqHelloworldApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jilinwula.rocketmq
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RocketmqHelloworldApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
